ef7f3311b8b2e8a5f4b287aab8cc1f67596d94e7
GhostLeader119 <cadecamp9@gmail.com>
2022-04-20 20:38:44 -0600

    Core elemts in place

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,19 @@
 function set_time() {
-const date_time = new Date();
-const current_month = date_time.getDay()
-const current_day = date_time.getDate()
-const current_year = date_time.getFullYear()
-const current_date = `${current_month}/${current_day}/${current_year}`
-document.querySelector('#year').textContent = current_date
+    console.log('System Check: last_modification called');
+    const date_time = new Date();
+    const current_month = date_time.getDay()
+    const current_day = date_time.getDate()
+    const current_year = date_time.getFullYear()
+    const current_date = `${current_month}/${current_day}/${current_year}`
+    document.querySelector('.year').textContent = current_date
+}
+function last_modifcation() {
+    console.log('System Check: last_modification called');
+    const modified_time = new Date(document.lastModified)
+    const outbound_message = `Last modifed: ${modified_time}`
+    document.querySelector('.last-mod').textContent = outbound_message
 }
 
 console.log('System Check: Javascript loaded');
 set_time()
+last_modifcation()
